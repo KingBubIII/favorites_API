a4c6bde6b529d2d52a2e3f0f644b61976005c0fb
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-08-29 23:58:41 -0600

    added comments to view functions

--- API/views.py
+++ API/views.py
@@ -3,7 +3,10 @@ from .models import Favorites
 from django.http import JsonResponse
 
 
+# Gets top X rated items in a category. Category and X defined by user in url
+# Returns list in JSON format
 def top_in_category(request, amount, category):
+    # Filters all favorite items based on rank and category
     top_favorites = get_list_or_404(
         Favorites.objects.defer("category")
         .filter(category__iexact=category)
@@ -15,22 +18,25 @@ def top_in_category(request, amount, category):
     return JsonResponse(top_favorites, safe=False)
 
 
+# Gets top X rated items in every category
+# Returns dictionary in JSON format
 def top_all(request, amount):
+    # Gets all categories from database
     all_categories = list(
         Favorites.objects.values_list("category", flat=True).distinct()
     )
     print(all_categories)
 
+    # Dictionary to be retuned
     top_favorites = {}
     for curr_category in all_categories:
-        top_in_category = get_list_or_404(
+        # Filters all favorite items based on rank and category
+        top_favorites[curr_category] = get_list_or_404(
             Favorites.objects.defer("category")
             .filter(category__iexact=curr_category)
             .filter(rank__lte=amount)
             .order_by("rank")
             .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
         )
-
-        top_favorites[curr_category] = top_in_category
 
     return JsonResponse(top_favorites)
