a82cc570906ae0e86a7dff5a4dce2dc5110fed96
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-09-02 20:38:05 -0600

    applied migration to tables

--- API/models.py
+++ API/models.py
@@ -20,7 +20,7 @@ class Suggestions(models.Model):
 
     name = models.CharField(max_length=100)
     comments = models.CharField(max_length=250, blank=True, null=True)
-    link = models.CharField(blank=True, null=True, unique=True)
+    link = models.URLField(blank=True, null=True, unique=True)
     category = models.CharField(max_length=50)
     suggester = models.CharField(max_length=50, blank=True, null=True)
     date_added = models.DateField(auto_now_add=True)
