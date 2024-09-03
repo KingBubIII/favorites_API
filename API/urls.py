2a0dd3f0df9c294bf60e95714ca4800e4b3f95ac
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-09-02 23:36:19 -0600

    added urls for latest item added, again having the option of further filtering by category optionally

--- API/urls.py
+++ API/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("top/<int:amount>/", views.top_all),
     path("top/<int:amount>/<str:category>/", views.top_in_category),
+    path("latest/", views.latest),
+    path("latest/<str:category>/", views.latest_by_category),
 ]
--- API/views.py
+++ API/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import HttpResponse, get_object_or_404, get_list_or_404
 from .models import Favorites
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.db.models import Subquery
 
 
 # Gets top X rated items in a category. Category and X defined by user in url
@@ -8,8 +10,7 @@ from django.http import JsonResponse
 def top_in_category(request, amount, category):
     # Filters all favorite items based on rank and category
     top_favorites = get_list_or_404(
-        Favorites.objects.defer("category")
-        .filter(category__iexact=category)
+        Favorites.objects.filter(category__iexact=category)
         .filter(rank__lte=amount)
         .order_by("rank")
         .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
@@ -32,11 +33,38 @@ def top_all(request, amount):
     for curr_category in all_categories:
         # Filters all favorite items based on rank and category
         top_favorites[curr_category] = get_list_or_404(
-            Favorites.objects.defer("category")
-            .filter(category__iexact=curr_category)
+            Favorites.objects.filter(category__iexact=curr_category)
             .filter(rank__lte=amount)
             .order_by("rank")
             .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
         )
 
     return JsonResponse(top_favorites)
+
+
+def latest(request):
+    subquery_timestamp = Favorites.objects.order_by("-date_added").values("date_added")[
+        :1
+    ]
+
+    most_recent_favorites = get_list_or_404(
+        Favorites.objects.filter(date_added__exact=Subquery(subquery_timestamp)).values(
+            "id", "name", "rank", "comments", "link", "added_by_suggestion", "category"
+        )
+    )
+
+    return JsonResponse(most_recent_favorites, safe=False)
+
+
+def latest_by_category(request, category):
+    subquery_timestamp = Favorites.objects.order_by("-date_added").values("date_added")[
+        :1
+    ]
+
+    most_recent_favorites = get_list_or_404(
+        Favorites.objects.filter(category__iexact=category)
+        .filter(date_added__exact=Subquery(subquery_timestamp))
+        .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
+    )
+
+    return JsonResponse(most_recent_favorites, safe=False)
