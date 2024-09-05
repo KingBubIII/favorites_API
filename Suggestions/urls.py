e30b2b9c485c79694b10b5dffdbc447921f5940d
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-09-04 18:39:12 -0600

    updated Suggestions views to actually work and use standard attributes in the response

--- Suggestions/urls.py
+++ Suggestions/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("top/<int:amount>/", views.top_all),
-    path("top/<int:amount>/<str:category>/", views.top_in_category),
     path("latest/", views.latest),
     path("latest/<str:category>/", views.latest_by_category),
-    path("random/", views.random_favorite),
-    path("random/<str:category>/", views.random_favorite_in_category),
-    path("search/<str:str_match>/", views.search_favorites),
-    path("search/<str:str_match>/<str:category>/", views.search_favorites_in_category),
+    path("random/", views.random_suggestion),
+    path("random/<str:category>/", views.random_suggestion_from_category),
+    path("search/<str:str_match>/", views.search_suggestion),
+    path("search/<str:str_match>/<str:category>/", views.search_suggestion_in_category),
 ]
--- Suggestions/views.py
+++ Suggestions/views.py
@@ -6,43 +6,13 @@ from django.db.models import Subquery
 from random import choice
 
 
-# Gets top X rated items in a category. Category and X defined by user in url
-# Returns list in JSON format
-def top_in_category(request, amount, category):
-    if request.method == "GET":
-        # Filters all favorite items based on rank and category
-        top_favorites = get_list_or_404(
-            Suggestions.objects.filter(category__iexact=category)
-            .filter(rank__lte=amount)
-            .order_by("rank")
-            .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
-        )
+# this is for the values method instead of hard-coding each attribute I need usually
+def _std_attributes(category=True):
+    attributes = ["id", "name", "comments", "link", "suggester"]
+    if category:
+        attributes.append("category")
 
-        return JsonResponse(top_favorites, safe=False)
-
-
-# Gets top X rated items in every category
-# Returns dictionary in JSON format
-def top_all(request, amount):
-    if request.method == "GET":
-        # Gets all categories from database
-        all_categories = list(
-            Suggestions.objects.values_list("category", flat=True).distinct()
-        )
-        print(all_categories)
-
-        # Dictionary to be retuned
-        top_favorites = {}
-        for curr_category in all_categories:
-            # Filters all favorite items based on rank and category
-            top_favorites[curr_category] = get_list_or_404(
-                Suggestions.objects.filter(category__iexact=curr_category)
-                .filter(rank__lte=amount)
-                .order_by("rank")
-                .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
-            )
-
-        return JsonResponse(top_favorites)
+    return attributes
 
 
 def latest(request):
@@ -54,15 +24,7 @@ def latest(request):
         most_recent_favorites = get_list_or_404(
             Suggestions.objects.filter(
                 date_added__exact=Subquery(subquery_timestamp)
-            ).values(
-                "id",
-                "name",
-                "rank",
-                "comments",
-                "link",
-                "added_by_suggestion",
-                "category",
-            )
+            ).values(*_std_attributes())
         )
 
         return JsonResponse(most_recent_favorites, safe=False)
@@ -77,13 +39,13 @@ def latest_by_category(request, category):
         most_recent_favorites = get_list_or_404(
             Suggestions.objects.filter(category__iexact=category)
             .filter(date_added__exact=Subquery(subquery_timestamp))
-            .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
+            .values(*_std_attributes(False))
         )
 
         return JsonResponse(most_recent_favorites, safe=False)
 
 
-def random_favorite(request):
+def random_suggestion(request):
     if request.method == "GET":
         # gets all ids from Suggestions table without loading other fields
         pks = Suggestions.objects.values_list("pk", flat=True)
@@ -91,15 +53,13 @@ def random_favorite(request):
         random_pk = choice(pks)
         # filters to only the chosen id and puts in a list to get values easier
         random_obj = get_list_or_404(
-            Suggestions.objects.filter(pk=random_pk).values(
-                "id", "name", "rank", "comments", "link", "added_by_suggestion"
-            )
+            Suggestions.objects.filter(pk=random_pk).values(*_std_attributes())
         )[0]
 
         return JsonResponse(random_obj)
 
 
-def random_favorite_in_category(request, category):
+def random_suggestion_from_category(request, category):
     if request.method == "GET":
         # gets all ids from category without loading other fields
         pks = Suggestions.objects.filter(category__iexact=category).values_list(
@@ -109,33 +69,29 @@ def random_favorite_in_category(request, category):
         random_pk = choice(pks)
         # filters to only the chosen id and puts in a list to get values easier
         random_obj = get_list_or_404(
-            Suggestions.objects.filter(pk=random_pk).values(
-                "id", "name", "rank", "comments", "link", "added_by_suggestion"
-            )
+            Suggestions.objects.filter(pk=random_pk).values(*_std_attributes(False))
         )[0]
 
         return JsonResponse(random_obj)
 
 
-def search_favorites(request, str_match):
+def search_suggestion(request, str_match):
     if request.method == "GET":
         # searches database name column
         results = get_list_or_404(
-            Suggestions.objects.filter(name__icontains=str_match).values(
-                "id", "name", "rank", "comments", "link", "added_by_suggestion"
-            )
+            Suggestions.objects.filter(name__icontains=str_match).values(*_std_attributes())
         )
 
         return JsonResponse(results, safe=False)
 
 
-def search_favorites_in_category(request, str_match, category):
+def search_suggestion_in_category(request, str_match, category):
     if request.method == "GET":
         # filters to category first then searches database name column
         results = get_list_or_404(
             Suggestions.objects.filter(category__iexact=category)
             .filter(name__icontains=str_match)
-            .values("id", "name", "rank", "comments", "link", "added_by_suggestion")
+            .values(*_std_attributes(False))
         )
 
         return JsonResponse(results, safe=False)
