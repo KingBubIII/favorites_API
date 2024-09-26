6560658b20704891169a8ed2e20a4c04ef031658
Caleb Richardson <richardsoncaleb60@gmail.com>
2024-09-25 22:04:58 -0600

    added introduction card

--- Docs/views.py
+++ Docs/views.py
@@ -5,16 +5,7 @@ from django.shortcuts import render
 def docs_view(request):
     url_info = load(open(URL_INFO_PATH))
 
-    # for url, details in url_info.items():
-    #     for example in details["examples"]:
-    #         for code, response in example["responses"].items():
-    #             test = dumps(response, indent=2)
-    #             example[code] = test
-
-    context = {}
-    context["urls"] = url_info["url_info"]
-
-    return render(request, "url_info.html", context)
+    return render(request, "url_info.html", url_info)
 
 def example(request, url_key):
     url_info_file = load(open(URL_INFO_PATH))
